28f6975d45ef9910181fc20d4f942d207d0d07fe
JAG <jag@helloimjag.com>
2017-08-17 19:22:39 -0700

    Adding Resources Schema

--- modules/resources/server/models/resource.server.model.js
+++ modules/resources/server/models/resource.server.model.js
@@ -10,10 +10,70 @@ var mongoose = require('mongoose'),
  * Resource Schema
  */
 var ResourceSchema = new Schema({
-  name: {
+  org: {
     type: String,
     default: '',
-    required: 'Please fill Resource name',
+    required: 'Please fill organization name',
+    trim: true
+  },
+  desc: {
+    type: String,
+    default: '',
+    required: 'Please fill description',
+    trim: true
+  },
+  web: {
+    type: String,
+    default: '',
+    required: 'Please fill website',
+    trim: true
+  },
+  addr: {
+    type: String,
+    default: '',
+    required: 'Please fill street',
+    trim: true
+  },
+  city: {
+    type: String,
+    default: '',
+    required: 'Please fill city',
+    trim: true
+  },
+  state: {
+    type: String,
+    default: '',
+    required: 'Please fill state',
+    trim: true
+  },
+  zip: {
+    type: String,
+    default: '',
+    required: 'Please fill zip',
+    trim: true
+  },
+  poc_name: {
+    type: String,
+    default: '',
+    required: 'Please fill point of contact name',
+    trim: true
+  },
+  poc_email: {
+    type: String,
+    default: '',
+    required: 'Please fill point of contact email',
+    trim: true
+  },
+  poc_line: {
+    type: String,
+    default: '',
+    required: 'Please fill point of contact line',
+    trim: true
+  },
+  tags: {
+    type: String,
+    default: '',
+    required: 'Please fill at least one tag',
     trim: true
   },
   created: {
