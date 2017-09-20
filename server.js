8e3e8f98098f34180c0f62960bab61727ac4734d
Sal P <salvatpalvat@gmail.com>
2017-09-20 18:44:32 -0400

    Connect to test database and create test api for home-page-about-us-section

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-'use strict';
+var mongo = require('mongodb').MongoClient;
+var app = require('express')();
+var url = 'mongodb://user1:password1@ds141524.mlab.com:41524/light-the-way';
 
-/**
- * Module dependencies.
- */
-var app = require('./config/lib/app');
-var server = app.start();
+
+mongo.connect(url, function(err, db) {
+    
+  if (err) throw err;
+  
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/modules/core/client/views/home.client.view.html');  
+  });
+    
+  app.get('/about-us', function (req, res) {
+    //Retrieve about-us document from the db
+    db.collection('general').find({ section: 'about-us' }).toArray(function (err, result) {
+      if (err) throw err;
+      var doc = result[0];
+      //Send the document to client
+      res.json(doc);
+      res.end();
+    })               
+  });
+  app.listen(5000);  
+    
+});
