32d022e3906e4f12c303b102cf8b7371ba8bb4b4
Sal P <salvatpalvat@gmail.com>
2017-09-21 16:24:06 -0400

    Test that controller and view successfully interact

--- modules/home-page-about-us.js
+++ modules/home-page-about-us.js
@@ -1,6 +1,18 @@
-console.log('HelloWorld!');
+/**
+    Create a controller that has a content assigned HelloWorld!
+        Input: 
+            a) Know how to create controller
+        
+        Steps:
+            1) Find out how to create controller
+            2) Create the controller
+**/
 
+var aboutUs = angular.module('about-us', []);
 
+aboutUs.controller('AboutUsController', function ($scope) {
+  $scope.content = 'HelloWorld!';
+});
 
 
 
