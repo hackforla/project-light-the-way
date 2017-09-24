2454997c656cb1a8eb8504f19edd72ccf569f0f6
Kyle Liu <kyle116@yahoo.com>
2017-09-24 15:01:07 -0700

    home page logo and title added

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.title = 'Welcome to Project Light The Way';
+    $scope.slogan = 'Find the resources you need, and discover ones you did not know about';
   }
 ]);
