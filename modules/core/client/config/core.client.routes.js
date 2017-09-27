e233c111873c11841ccfb515d105b4fa1cb4621d
JAG <jag@helloimjag.com>
2017-09-26 20:05:11 -0700

    state home modified
    controller added as vm

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -1,42 +1,46 @@
-'use strict';
+(function() {
+  'use strict';
 
-// Setting up route
-angular.module('core').config(['$stateProvider', '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+  // Setting up route
+  angular.module('core').config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
 
-    // Redirect to 404 when route not found
-    $urlRouterProvider.otherwise(function($injector, $location) {
-      $injector.get('$state').transitionTo('not-found', null, {
-        location: false
+      // Redirect to 404 when route not found
+      $urlRouterProvider.otherwise(function($injector, $location) {
+        $injector.get('$state').transitionTo('not-found', null, {
+          location: false
+        });
       });
-    });
 
-    // Home state routing
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'modules/core/client/views/home.client.view.html'
-      })
-      .state('not-found', {
-        url: '/not-found',
-        templateUrl: 'modules/core/client/views/404.client.view.html',
-        data: {
-          ignoreState: true
-        }
-      })
-      .state('bad-request', {
-        url: '/bad-request',
-        templateUrl: 'modules/core/client/views/400.client.view.html',
-        data: {
-          ignoreState: true
-        }
-      })
-      .state('forbidden', {
-        url: '/forbidden',
-        templateUrl: 'modules/core/client/views/403.client.view.html',
-        data: {
-          ignoreState: true
-        }
-      });
-  }
-]);
+      // Home state routing
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'modules/core/client/views/home.client.view.html',
+          controller: 'HomePageController as vm'
+        })
+        .state('not-found', {
+          url: '/not-found',
+          templateUrl: 'modules/core/client/views/404.client.view.html',
+          data: {
+            ignoreState: true
+          }
+        })
+        .state('bad-request', {
+          url: '/bad-request',
+          templateUrl: 'modules/core/client/views/400.client.view.html',
+          data: {
+            ignoreState: true
+          }
+        })
+        .state('forbidden', {
+          url: '/forbidden',
+          templateUrl: 'modules/core/client/views/403.client.view.html',
+          data: {
+            ignoreState: true
+          }
+        });
+    }
+  ]);
+
+}());
