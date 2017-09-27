ca88373a4d66b8bbaf9577b8202949c415377c67
JAG <jag@helloimjag.com>
2017-09-26 20:42:24 -0700

    Controller added to core/controller and modified

--- modules/home-page-about-us.js
+++
@@ -1,7 +0,0 @@
-var aboutUs = angular.module('about-us', []);
-
-aboutUs.controller('AboutUsController', function ($scope, $http) {
-  $http.get('about-us/content').then(function(response) {
-    $scope.content = response.data.content;    
-  })
-});
