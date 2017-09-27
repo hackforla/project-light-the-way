5b798e7acb6adeaf04d9093570d2645863d6ce77
JAG <jag@helloimjag.com>
2017-09-26 21:48:03 -0700

    LTW details added
    Removing additional options

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'LTW',
+    description: 'A website that provides a easier way to connect veterans to their benefits and other resources.',
+    keywords: 'Veteran, Vets, Military, Service, Benefits, Resources, Help, Support',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
