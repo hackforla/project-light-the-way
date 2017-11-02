2c726118937e46deedb22ad8e89023024ca8af0a
JAG <jag@helloimjag.com>
2017-11-01 21:35:18 -0700

    Removing unused authentication

--- modules/resources/client/controllers/resources.client.controller.js
+++ modules/resources/client/controllers/resources.client.controller.js
@@ -6,38 +6,19 @@
     .module('resources')
     .controller('ResourcesController', ResourcesController);
 
-  ResourcesController.$inject = ['$location', '$state', '$window', 'Authentication', 'ResourcesService'];
+  ResourcesController.$inject = ['$location', '$state', '$window', 'ResourcesService'];
 
-  function ResourcesController ($location, $state, $window, Authentication, resource) {
+  function ResourcesController ($location, $state, $window, resource) {
     var vm = this;
-
-    vm.authentication = Authentication;
     vm.error = null;
     vm.form = {};
     vm.form.reset = reset;
-    // vm.remove = remove;
     vm.save = save;
     var d = 'delete';
     var id = $state.params.resourceId;
     if(id){
       get();
     }
-    // else{
-    //   vm.resource = {
-    //     org: d,
-    //     desc: d,
-    //     web:'www.'+d+'.com',
-    //     poc_name: d,
-    //     poc_email: d+'@'+d+'.com',
-    //     poc_line: '1231231234',
-    //     tags:d,
-    //     city:d,
-    //     state:'ca',
-    //     zip:'00000',
-    //     addr:'123 Main St'
-    //   };
-    // }
-
 
     function get(){
       resource.getResource(id).get(success, err);
@@ -54,8 +35,8 @@
     }
 
     function success(d){
-      vm.resource = d;
-      vm.resource = d;
+      console.log(d);
+      vm.resource = d.data;
     }
 
     function err(e){
@@ -67,39 +48,5 @@
       $location.path('/r/form');
     }
 
-
-
-    // Remove existing Resource
-    // function remove() {
-    //   if ($window.confirm('Are you sure you want to delete?')) {
-    //     vm.resource.$remove($state.go('resources.list'));
-    //   }
-    // }
-
-    // Save Resource
-    // function save(isValid) {
-    //   console.log(isValid);
-    //   if (!isValid) {
-    //     $scope.$broadcast('show-errors-check-validity', 'vm.form.resourceForm');
-    //     return false;
-    //   }
-    //
-    //   // TODO: move create/update logic to service
-    //   if (vm.resource._id) {
-    //     vm.resource.$update(successCallback, errorCallback);
-    //   } else {
-    //     vm.resource.$save(successCallback, errorCallback);
-    //   }
-    //
-    //   function successCallback(res) {
-    //     $state.go('resources.edit', {
-    //       resourceId: res._id
-    //     });
-    //   }
-    //
-    //   function errorCallback(res) {
-    //     vm.error = res.data.message;
-    //   }
-    // }
   }
 }());
