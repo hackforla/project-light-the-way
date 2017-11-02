69b8eec393016590ad180651d238bca31829661b
JAG <jag@helloimjag.com>
2017-11-01 19:36:02 -0700

    Clean up

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -10,18 +10,9 @@
   function HomePageController(Authentication, content, categories, resources) {
     var vm = this;
 
-    // This provides Authentication context.
-    vm.authentication = Authentication;
     vm.categories = [];
     vm.featured = [];
     vm.recent = [];
-
-    // For home view
-    vm.home = {};
-    vm.home.aboutus = content.getAboutUs();
-    vm.home.title = 'Welcome to Project Light The Way';
-    vm.home.slogan = 'Find the resources you need, and discover ones you did not know about';
-    vm.home.logo = '/modules/core/client/img/brand/logo1.png';
 
     categories.getAll().get(function(d){
       vm.categories = d.data;
@@ -29,8 +20,6 @@
 
     resources.getNew().get(function(d){
       vm.recent = d.data;
-
-      console.log(d.data);
     });
 
     resources.getFeatured().get(function(d){
