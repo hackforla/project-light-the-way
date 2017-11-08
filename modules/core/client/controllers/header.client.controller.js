dde32649597f074009f9c14f8036d12d1e4b05c1
JAG <jag@helloimjag.com>
2017-11-07 22:34:34 -0800

    Updating style

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,23 +1,68 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
-  function ($scope, $state, Authentication, Menus) {
-    // Expose view variables
-    $scope.$state = $state;
-    $scope.authentication = Authentication;
+angular.module('core').controller('HeaderController', HeaderController);
+HeaderController.$inject = ['$window', '$state', 'Authentication', 'Menus'];
+function HeaderController($window, $state, Authentication, Menus) {
+  var vm = this;
+  var nav;
 
-    // Get the topbar menu
-    $scope.menu = Menus.getMenu('topbar');
+  vm.nav = {};
+  vm.nav.scroll = 0;
+  vm.nav.dir = '';
 
-    // Toggle the menu items
-    $scope.isCollapsed = false;
-    $scope.toggleCollapsibleMenu = function () {
-      $scope.isCollapsed = !$scope.isCollapsed;
-    };
+  // Expose view variables
+  vm.$state = $state;
+  vm.authentication = Authentication;
 
-    // Collapsing the menu after navigation
-    $scope.$on('$stateChangeSuccess', function () {
-      $scope.isCollapsed = false;
-    });
+  // Get the topbar menu
+  vm.menu = Menus.getMenu('topbar');
+
+  // Toggle the menu items
+  vm.isCollapsed = false;
+  vm.toggleCollapsibleMenu = function () {
+    vm.isCollapsed = !vm.isCollapsed;
+  };
+  angular.element($window).addClass('.home');
+  angular.element(document.querySelector('.home')).bind('scroll', function(){
+    scroll();
+  });
+
+  function scroll(){
+    var nav = document.querySelector('.home-nav');
+    vm.nav.height = document.querySelector('.home-hero').offsetHeight;
+    vm.nav.dir = (($window.scrollY - vm.nav.scroll) < 0) ? 'up' : 'down';
+    if(vm.nav.dir === 'down'){
+      if(vm.nav.scroll > vm.nav.height){
+        showBG(nav);
+        showNAV(nav);
+      }else{
+        hideBG(nav);
+        showNAV(nav);
+      }
+    }else{
+      if(vm.nav.scroll > vm.nav.height){
+        hideNAV(nav);
+        showNAV(nav);
+      }else{
+        hideBG(nav);
+        showNAV(nav);
+      }
+    }
+    vm.nav.scroll = $window.scrollY;
   }
-]);
+
+  function showBG(e){
+    angular.element(e).addClass('nav-bg');
+  }
+
+  function hideBG(e){
+    angular.element(e).removeClass('nav-bg');
+  }
+
+  function showNAV(e){
+    angular.element(e).removeClass('home-nav-hide');
+  }
+  function hideNAV(e){
+    angular.element(e).addClass('home-nav-hide');
+  }
+}
