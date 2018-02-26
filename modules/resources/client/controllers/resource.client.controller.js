060978bcb3c36db71a155846c02a20373af5426d
JAG <jag@helloimjag.com>
2018-02-26 02:35:46 -0800

    Temp placeholder for distance services.

--- modules/resources/client/controllers/resource.client.controller.js
+++ modules/resources/client/controllers/resource.client.controller.js
@@ -10,14 +10,15 @@
   function ResourceReadController($state, resources, checklistStorage) {
     var vm = this;
     vm._id = $state.params.resourceId;
-
     vm.checklist = checklistStorage.get();
-
+    vm.distance = false; // Will get toggle on filter system change
+    vm.distance = true; // temp
 
     vm.inChecklist = vm.checklist.indexOf(vm._id) !== -1;
 
     resources.getResource(vm._id).get(function(d){
       vm.resource = d.data;
+      vm.resource.distance = '1.2 miles away';
     });
   }
 }());
